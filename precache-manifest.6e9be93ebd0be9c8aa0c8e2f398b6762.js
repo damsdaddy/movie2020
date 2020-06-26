78b016c07c9c0f7e5e475dc3110b1dc848dd2ae7
damsdaddy <boardgod@korea.ac.kr>
2020-06-26 19:55:13 +0900

    Updates

---
+++ precache-manifest.6e9be93ebd0be9c8aa0c8e2f398b6762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77fadbd86b0c69b68384adf1f488ceb",
+    "url": "/movie2020/index.html"
+  },
+  {
+    "revision": "2174e6e94efb45482621",
+    "url": "/movie2020/static/css/main.9474c6ac.chunk.css"
+  },
+  {
+    "revision": "1d5eb0c81536ab5e791f",
+    "url": "/movie2020/static/js/2.b53af260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie2020/static/js/2.b53af260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2174e6e94efb45482621",
+    "url": "/movie2020/static/js/main.3b815f2e.chunk.js"
+  },
+  {
+    "revision": "cd78b73b76761143ffe9",
+    "url": "/movie2020/static/js/runtime-main.e94dd631.js"
+  }
+]);
